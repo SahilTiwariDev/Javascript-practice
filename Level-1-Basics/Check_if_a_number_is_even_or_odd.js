63ce02ddf0ecdf84db8397d4497ef956fedfd78b
SahilTiwariDev <sahiltiwari.contactme@gmail.com>
2025-05-18 14:12:46 +0530

    Program 2

---
+++ Level-1-Basics/Check_if_a_number_is_even_or_odd.js
@@ -0,0 +1,8 @@
+function oddEven(params) {
+  if (params % 2 === 0) {
+    console.log(`${params} is an even number.`);
+  } else {
+    console.log(`${params} is an odd number.`);
+  }
+}
+oddEven(5); // Give your input here
