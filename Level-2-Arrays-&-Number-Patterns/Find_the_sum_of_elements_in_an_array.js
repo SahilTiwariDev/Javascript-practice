69f92949ab145fe082fb55176ef031a9fa7c2608
SahilTiwariDev <sahiltiwari.contactme@gmail.com>
2025-06-02 23:31:56 +0530

    Program 21

---
+++ Level-2-Arrays-&-Number-Patterns/Find_the_sum_of_elements_in_an_array.js
@@ -0,0 +1,8 @@
+function sumOfElements(params) {
+  let sum = 0;
+  for (let index = 0; index < params.length; index++) {
+    sum += params[index];
+  }
+  console.log(`The sum of elements in this array is ${sum}`);
+}
+sumOfElements([1, 2, 3, 4, 5, 6]);
